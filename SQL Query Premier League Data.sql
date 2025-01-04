8084265f3f64af0708c5fc6b810052dbbd49d10d
omridavid777 <omridavid777@gmail.com>
2025-01-04 18:40:13 +0200

    Add files via upload

---
+++ SQL Query Premier League Data.sql
@@ -0,0 +1,26 @@
+select HomeTeam AS Team, AVG(FTHG) AS AVG_HomeGoals, AVG(FTAG) AS AVG_AwayGoals 
+from dbo.E0$
+GROUP BY HomeTeam;
+
+
+
+SELECT 
+    Team,
+    SUM(HomeGoals) AS HomeGoals,
+    SUM(AwayGoals) AS AwayGoals,
+    SUM(HomeGoals) + SUM(AwayGoals) AS TotalGoals
+FROM (
+    SELECT 
+        HomeTeam AS Team, 
+        FTHG AS HomeGoals,
+        0 AS AwayGoals
+    FROM dbo.E0$
+    UNION ALL
+    SELECT 
+        AwayTeam AS Team, 
+        0 AS HomeGoals,
+        FTAG AS AwayGoals
+    FROM dbo.E0$
+) AS Combined
+GROUP BY Team
+ORDER BY TotalGoals DESC;
